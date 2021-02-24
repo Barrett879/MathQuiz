33f228b02fd35fe8d0ab50e69105af2c59ef54c1
Barrett879 <77691102+Barrett879@users.noreply.github.com>
2021-02-24 09:52:25 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
  $(document).ready(function() {
 	  
-$("p").click(function(){
-  $(".disappear").toggle();
-});
   
 	$("#imagetoggler").click(function(){
-		$("img").toggle();
+		$("#imagetoggler").text("Correct!");
 	});
+	$("#imagetoggler1").click(function(){
+		$("#imagetoggler1").text("Incorrect!");
+	});
+	$("#imagetoggler2").click(function(){
+		$("#imagetoggler2").text("Incorrect!");
+	});
+	$("#imagetoggler0").click(function(){
+		$("#imagetoggler0").text("Incorrect!");
+	});
+	$("#imagetoggler4").click(function(){
+		$("#imagetoggler4").text("Thanks for taking my test!");
+	});
+	
+	
 });
